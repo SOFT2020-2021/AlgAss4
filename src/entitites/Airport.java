2f2e379959faee11c5b9a7ac097e3f3708c7a3f8
JonasGroenbek <jonasgroenbek@gmail.com>
2020-05-08 12:41:54 +0200

    init

---
+++ src/entitites/Airport.java
@@ -0,0 +1,70 @@
+package entitites;
+
+public class Airport {
+
+    private String code;
+    private String name;
+    private String city;
+    private String country;
+    private String latitude;
+    private String longitude;
+
+    public Airport() {}
+
+    public Airport(String code, String name, String city, String country, String latitude, String longitude) {
+        this.code = code;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,7 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        MyGraph mg = new MyGraph();
+    }
+}
